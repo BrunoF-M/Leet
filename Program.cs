3c247fff73e1372d20cb29459a41a0bdeb582c45
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-09-18 22:18:35 -0300

    Resolução do Exercício

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            string texto;
+
+            Console.Write("Tradutor para"); 
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write(" \"leet\" ");
+            Console.ResetColor();
+            Console.Write("digite oque deseja traduzir: ");
+            texto = Console.ReadLine();
+
+            string texto2 = texto.Replace("A","4").Replace("a","4").Replace("E","3").Replace("e","3").Replace("I","1").Replace("i","1").Replace("L","1").Replace("l","1").Replace("O","0").Replace("o","0").Replace("T","7").Replace("t","7").Replace("S","5").Replace("s","5");
+
+            Console.Write($"Texto a ser traduzido...: ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write($"{texto2}");
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Aperte qualque tecla para Finalizar... ");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
